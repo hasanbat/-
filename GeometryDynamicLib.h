252cc106f6dc5ed1cb6a7438919e705f72a6cf85
hasanbat <hasanbatraev9@icloud.com>
2025-12-18 00:28:11 +0300

    Create GeometryDynamicLib.h

---
+++ GeometryDynamicLib.h
@@ -0,0 +1,38 @@
+#ifndef GEOMETRY_DYNAMIC_LIB_H
+#define GEOMETRY_DYNAMIC_LIB_H
+
+#include <cmath>
+
+namespace Geometry {
+    class Square {
+    private:
+        double side;
+    public:
+        Square(double s);
+        double area() const;
+        double perimeter() const;
+        void display() const;
+    };
+
+    class Circle {
+    private:
+        double radius;
+    public:
+        Circle(double r);
+        double area() const;
+        double perimeter() const;
+        void display() const;
+    };
+
+    class Triangle {
+    private:
+        double side;
+    public:
+        Triangle(double s);
+        double area() const;
+        double perimeter() const;
+        void display() const;
+    };
+}
+
+#endif
