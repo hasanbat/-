937add034df0ed893943e0dbf7edd9c33f09086e
hasanbat <hasanbatraev9@icloud.com>
2025-12-18 00:27:25 +0300

    Create GeometryStaticLib.h

---
+++ GeometryStaticLib.h
@@ -0,0 +1,13 @@
+#ifndef GEOMETRY_STATIC_LIB_H
+#define GEOMETRY_STATIC_LIB_H
+
+namespace Geometry {
+    double squareArea(double side);
+    double squarePerimeter(double side);
+    double circleArea(double radius);
+    double circlePerimeter(double radius);
+    double triangleArea(double side);
+    double trianglePerimeter(double side);
+}
+
+#endif
